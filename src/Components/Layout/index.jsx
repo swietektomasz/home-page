5a395ec9935ad775c5901d1b1bfde9eee61e525b
swietektomasz <swietek.tomasz@protonmail.com>
2019-10-30 21:11:26 +0100

    Basic layout setup

---
+++ src/Components/Layout/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './layout.css';
+
+function Layout() {
+  return (
+    <div className="layout">
+      <div className="content">This will be the main content</div>
+      <div className="sidebar">This will be the sidebar</div>
+    </div>
+  );
+}
+
+export default Layout;
